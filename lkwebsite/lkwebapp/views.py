39e3f5ebadb0a938b5f5f4f769abdc3bfb6e63e8
a6573 <a6573@DESKTOP-P2M7SMI>
2018-12-16 23:21:48 +0800

    分页器

--- lkwebsite/lkwebapp/views.py
+++ lkwebsite/lkwebapp/views.py
@@ -4,17 +4,39 @@ from .models import lkModel,lkModelType
 import json
 sys.path.append('..')
 from .requestUilts.requestModel import requestClass
-
+from django.core.paginator import Paginator
 # Create your views here.
 
 def interFaceList(request):
-    interFaces=lkModel.objects.all()
-    interFaces_type=lkModelType.objects.all()
-    print(interFaces_type)
+    
+    interFaces_all_list=lkModel.objects.all()
+    paginator=Paginator(interFaces_all_list,4)
+    page_num=request.GET.get('page',1)
+    page_of_interFaces=paginator.get_page(page_num)
+    currentr_page_number=page_of_interFaces.number
+
+    page_range= [currentr_page_number - 2,currentr_page_number - 1,currentr_page_number,currentr_page_number + 1,currentr_page_number + 2]
+    
+    page_range=list(range(max(currentr_page_number - 2 ,1),currentr_page_number))+ \
+                list(range(currentr_page_number,min(currentr_page_number + 2,paginator.num_pages)+ 1))
+    #interFaces=lkModel.objects.all()
+    #interFaces=page_of_interFaces.object_list
+    if page_range[0] -1 >=2:
+        page_range.insert(0,'...')
+    if paginator.num_pages -page_range[-1]>=2:
+        page_range.append('...')
+
+
+
+    if page_range[0]!=1:
+        page_range.insert(0,1)
+    if page_range[-1]!=paginator.num_pages:
+        page_range.append(paginator.num_pages)
     context={}
-    context['interFaces']=interFaces
-    print(interFaces)
-    context['interFaces_type']=interFaces_type
+    context['page_of_interFaces']=page_of_interFaces
+    context['page_range']=page_range
+    #print(interFaces)
+    context['interFaces_type']=lkModelType.objects.all()
     return render(request,'interFace_list.html',locals())
 #     return render_to_response('interFace_list.html', context)
 
